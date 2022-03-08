9a4716ad055e75ef474bf215601a5aeb3fc94132
Dnyaneshwar <d.suryawanshi123@gmail.com>
2022-03-08 16:39:40 +0530

    Code commit for variable name rename index -> startIndex.

--- ConvertPlainFileToCSV/Program.cs
+++ ConvertPlainFileToCSV/Program.cs
@@ -11,7 +11,7 @@ namespace ConvertPlainFileToCSV
             string textFilePath = @"c:\POC\DemoPOC\SampleText.txt";
             string csvfilePath = @"c:\POC\DemoPOC\output.csv";
 
-            int index = 0;
+            int startIndex = 0;
             int[] arr = { 9,3,10,6,4,5,10,1,16,35,6,1,26,1,9,1,16,1,16,2,8,1,2,1,16,1,16,1,5,1,4,1,8,1,20,1,8,1 };
             string fieldNames = "LUW ID,Record Type,Source ID,Run Number,Company ID,Event ID,Fund Code,Filler01,AMOUNTSTR,TRANSDATESTR,Cheque Number,Filler02,Payee Name,Filler03,Payee Postcode,Filler04,Payee Reference,Filler05,Agent ID,Filler06,Treaty ID,Filler07,Premium Type,Filler08,Scheme ID,Filler09,Wrapper Number,Filler10,Reason Code,Filler11,PRAS Scheme,Filler12,Product Code,Filler13,Incident Number,Filler14,Suspense Entry ID,Filler15,Claim Bonus Type,Filler16,Payroll Class Id,Filler17,PCRN,Filler18,Unique Receipt No,Filler19,Claim Reference,Filler20,Payment Plan Reference ,Filler21,Payroll ID,Filler22";
             
@@ -34,20 +34,20 @@ namespace ConvertPlainFileToCSV
                     {
                         if (i == arr.Length)
                         {
-                            var results = line.Substring(index, arr[arr.Length - 1]);
+                            var results = line.Substring(startIndex, arr[arr.Length - 1]);
                             File.AppendAllText(csvfilePath, results);
                         }
                         else
                         {
-                            var results = line.Substring(index, arr[i]);
-                            index = index + arr[i];
+                            var results = line.Substring(startIndex, arr[i]);
+                            startIndex = startIndex + arr[i];
                             File.AppendAllText(csvfilePath, results + ",");
 
                         }
 
                     }
                     System.Console.WriteLine(line);
-                    index = 0;
+                    startIndex = 0;
                 }
 
                 // adding new line once one line reading completed.
