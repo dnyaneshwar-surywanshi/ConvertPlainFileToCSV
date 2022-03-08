3bb9c3ee09f50a3a0db5a0177a945c1f7a7ff09b
Dnyaneshwar <d.suryawanshi123@gmail.com>
2022-03-08 16:34:38 +0530

    Code commit for text file line removed.

--- ConvertPlainFileToCSV/Program.cs
+++ ConvertPlainFileToCSV/Program.cs
@@ -10,12 +10,21 @@ namespace ConvertPlainFileToCSV
         {
             string textFilePath = @"c:\POC\DemoPOC\SampleText.txt";
             string csvfilePath = @"c:\POC\DemoPOC\output.csv";
+
             int index = 0;
             int[] arr = { 9,3,10,6,4,5,10,1,16,35,6,1,26,1,9,1,16,1,16,2,8,1,2,1,16,1,16,1,5,1,4,1,8,1,20,1,8,1 };
             string fieldNames = "LUW ID,Record Type,Source ID,Run Number,Company ID,Event ID,Fund Code,Filler01,AMOUNTSTR,TRANSDATESTR,Cheque Number,Filler02,Payee Name,Filler03,Payee Postcode,Filler04,Payee Reference,Filler05,Agent ID,Filler06,Treaty ID,Filler07,Premium Type,Filler08,Scheme ID,Filler09,Wrapper Number,Filler10,Reason Code,Filler11,PRAS Scheme,Filler12,Product Code,Filler13,Incident Number,Filler14,Suspense Entry ID,Filler15,Claim Bonus Type,Filler16,Payroll Class Id,Filler17,PCRN,Filler18,Unique Receipt No,Filler19,Claim Reference,Filler20,Payment Plan Reference ,Filler21,Payroll ID,Filler22";
+            
+            // Adding all fields name in csv file as per the GL Detail Record .
             File.AppendAllText(csvfilePath, fieldNames);
             File.AppendAllText(csvfilePath, Environment.NewLine);
-            foreach (string line in System.IO.File.ReadLines(textFilePath))
+
+            //reading all text file lines
+            var lines = System.IO.File.ReadAllLines(textFilePath);
+
+            //removing last line from the text file.
+            lines = lines.Take(lines.Length - 1).ToArray();
+            foreach (string line in lines)
             {
                
                 if (!string.IsNullOrEmpty(line))
@@ -41,6 +50,7 @@ namespace ConvertPlainFileToCSV
                     index = 0;
                 }
 
+                // adding new line once one line reading completed.
                 File.AppendAllText(csvfilePath, Environment.NewLine);
 
             } 
